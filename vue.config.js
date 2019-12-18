fe1a120347cd5dc787afee1659a0c6cb291b3969
Waidhoferj <jwaidhof@calpoly.edu>
2019-12-17 23:34:16 -0800

    added pwa configuration

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    name: "CSAI Recorder",
+    themeColor: "#FFF",
+    msTileColor: "#FFF",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "default",
+    manifestOptions: {
+      background_color: "#FFF"
+    }
+  }
+};
