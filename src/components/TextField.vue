ff2b6704630a38bf0fe6675fbf0afc3847269cc9
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-11 21:23:56 -0800

    added error checking

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="text-field" :class="{ drift: !value.length }">
+  <div class="text-field" :class="{ invalid }">
     <p class="label" :class="{ shown: showLabel }">{{ label }}</p>
     <input
       :value="value"
       @input="$emit('input', $event.target.value)"
+      @blur="$emit('blur', $event)"
       :placeholder="label"
-      type="text"
+      :type="type"
     />
+    <p class="label error" :class="{ shown: invalid }">{{ errorMessage }}</p>
   </div>
 </template>
 
@@ -17,8 +19,18 @@ export default {
       type: String,
       required: true
     },
+    type: {
+      type: String,
+      default: "text"
+    },
+    invalid: {
+      type: Boolean,
+      default: false
+    },
+    errorMessage: String,
     value: String
   },
+
   computed: {
     showLabel() {
       return this.value.length;
@@ -29,11 +41,17 @@ export default {
 
 <style lang="scss">
 .text-field {
-  transition: transform 0.5s;
-  &.drift {
-    transform: translateY(-14px);
+  width: min-content;
+  transition: transform 0.5s, background 0.5s;
+  border-radius: 7px;
+  background: transparent;
+
+  &.invalid {
+    background: #f3080869;
   }
+
   .label {
+    margin-left: 5px;
     font-size: 12px;
     font-weight: 700;
     transition: all 0.7s;
@@ -42,23 +60,37 @@ export default {
     transform: translateY(100%);
     pointer-events: none;
     clip-path: inset(0 0 100% 0);
+    text-align: left;
 
-    &.shown {
+    &.shown,
+    &.error.shown {
       opacity: 1;
       transform: translateY(0%);
       clip-path: inset(0 0 0% 0);
       pointer-events: all;
     }
+
+    &.error {
+      font-weight: 500;
+      transform: translateY(-100%);
+      clip-path: inset(100% 0 0 0);
+    }
   }
   input {
+    text-align: left;
     font-family: inherit;
     font-size: inherit;
+    font-weight: 500;
     border: none;
-    padding: 5px;
+    background: white;
+    border-radius: 7px;
+    margin: 0;
+    padding: 7px;
+    color: var(--dark);
   }
   ::placeholder {
-    color: var(--secondary);
-    font-weight: 700;
+    color: rgba(44, 44, 44, 0.486);
+    font-weight: 500;
   }
 }
 </style>
