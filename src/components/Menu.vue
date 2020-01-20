efd2de6b254f4f016413b7135374c096bb9f9648
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-19 19:08:40 -0800

    added a menu component

---
+++ src/components/Menu.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="menu">
+    <router-link to="/" class="choice">
+      <ion-icon name="mic"></ion-icon>
+    </router-link>
+    <router-link to="phrases" class="choice">
+      <ion-icon name="quote"></ion-icon>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {}
+};
+</script>
+
+<style lang="scss">
+.menu {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: var(--menu-height);
+  background: var(--dark);
+  box-shadow: 0px 0 30px rgba(0, 0, 0, 0.164);
+  padding: 10px 20px;
+
+  ion-icon {
+    height: 40px;
+    margin: auto;
+    padding: 0;
+    fill: white;
+  }
+
+  .choice {
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    color: white;
+    transition: opacity 0.5s, transform 0.5s;
+    padding: 0 20px;
+
+    &:active {
+      transform: scale(0.95);
+      opacity: 0.7;
+    }
+  }
+}
+</style>
