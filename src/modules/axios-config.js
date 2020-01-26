18afaa28ca8421b93fad0e8bbd64e7fb10a6b227
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-26 14:00:22 -0800

    added cross site access

--- src/modules/axios-config.js
+++ src/modules/axios-config.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:5000/";
+axios.defaults.baseURL = "http://192.99.12.62:5001/";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
