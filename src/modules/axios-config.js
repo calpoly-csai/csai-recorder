c9f7ffb69d1a38c856277282acc19975ea07c123
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-09 21:23:03 -0800

    updated server config

--- src/modules/axios-config.js
+++ src/modules/axios-config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.99.12.62:7000/";
+axios.defaults.baseURL = "https://192.99.12.62:7000";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
