05582d90fd54c614442492382ad6ad1e59602209
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-10 10:41:08 -0800

    updated base url

--- src/modules/axios-config.js
+++ src/modules/axios-config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://192.99.12.62:7000";
+axios.defaults.baseURL = "https://data-temp.calpolycsai.com:7000";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
