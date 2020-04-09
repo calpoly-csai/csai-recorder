4451f7520da3de34a3be7894357c3fdab3fa9fa9
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2020-04-08 22:13:36 -0700

    Initial help page template

---
+++ src/views/Help.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="help page">
+    <div class="content">
+      <h1 class="title">Help</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss">
+.help {
+  .content {
+    .title {
+      margin: 20px 0;
+    }
+  }
+}
+</style>
