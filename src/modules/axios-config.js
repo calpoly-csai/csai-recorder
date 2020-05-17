969dc9c63aa8b891c97ef00d6448279894624997
Waidhoferj <jwaidhof@calpoly.edu>
2020-05-17 13:50:20 -0700

    updated to gcloud

--- src/modules/axios-config.js
+++ src/modules/axios-config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://data-temp.calpolycsai.com:7000";
+axios.defaults.baseURL = "https://nimbus.api.calpolycsai.com";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
